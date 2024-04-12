79a5b230efd9d62861d14ab3d3a7824f539124b5
Yuktha karkera <yukthakarkera90@gmail.com>
2024-04-12 10:12:22 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages,setup
+
+
+setup{
+name='mlproject',
+version='0.0.1',
+author='Yuktha Karkera'
+author_email='yukthakarker21@gmail.com',
+packages=find_packages(),
+install_requirements=get_requirements('requirement.txt')
+
+
+}
+
